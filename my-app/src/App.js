5499192637595d535b112ebea8a9c67aa7c49dcc
sinjeeli <sinjeeli@gmail.com>
2023-07-10 15:34:37 -0400

    semi-working state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,49 @@
 import React, {useEffect, useState} from 'react';
 import './index.css';
 import SearchFormHTML from './components/SearchForm';
+import apiKey from './components/config';
 
 const App = () => {
-  const [img, setImg] = useState([]);
-  useEffect(()=>{fetch('');},[]);
+  const [photos, setPhotos] = useState([]);
+  useEffect(() => {
+    const fetchCatPhotos = async () => {
+      try {
+        const response = await fetch(
+          `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=cats&per_page=24&format=json&nojsoncallback=1`
+        );
+        if (response.ok) {
+          const data = await response.json();
+          setPhotos(data.photos.photo);
+        } else {
+          throw new Error('Request failed with status ' + response.status);
+        }
+      } catch (error) {
+        console.error('Error fetching cat photos:', error);
+      }
+    };
+  
+    fetchCatPhotos();
+  }, []);
+  
   //
   return (
     <div className='container'>
       <SearchFormHTML />
-
-
+      <div className="photo-container">
+      <h2>Cat Photos</h2>
+      <ul>
+        {photos.map((photo) => (
+          <li key={photo.id}>
+            <img
+              src={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
+              alt={photo.title}
+            />
+          </li>
+        ))}
+      </ul>
     </div>
- 
-  );
-};
+  </div>
+ );
 
+        }
 export default App;
---
+++ my-app/src/components/config.js
@@ -0,0 +1,2 @@
+const apiKey = '33763372d1bb54912d1fc2c00d3d7e33';
+export default apiKey;
